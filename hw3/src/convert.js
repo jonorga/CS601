766d8a4ae2380a4843fdc827122d16f563dfbe34
jonorgan <jonorgan26@gmail.com>
2024-02-07 22:27:43 -0500

    Updated hw3 to run with module

--- hw3/src/convert.js
+++ hw3/src/convert.js
@@ -1,18 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-	function fToC(fVal) {
-		if (isNaN(Number(fVal)) || Number(fVal) > 250 || Number(fVal) < -250)
-			return "invalid input";
-		return Math.floor(((fVal - 32) * 5) / 9);
-	}
-	const slider_input = document.getElementById("slider_input");
-	const temp_output = document.getElementById("slider_output");
-	const temp_output2 = document.getElementById("slider_output2");
-	temp_output.innerHTML = "Celcius temp: " + fToC(slider_input.value);
-	temp_output2.innerHTML = "F temp: " + slider_input.value;
+export function convertTemp() {
+	document.addEventListener("DOMContentLoaded", function() {
+		const temp_input = document.getElementById("temperature_input");
+		const temp_output_f = document.getElementById("output_f");
+		const temp_output_c = document.getElementById("output_c");
+
+		function fToC(fVal) {
+			// The line below validates the entry to be a number in the correct range or returns invalid input
+			if (isNaN(Number(fVal)) || Number(fVal) > 250 || Number(fVal) < -250)
+				return "invalid input";
+			if (fVal == "")
+				return "no value entered";
+			return Math.floor(((fVal - 32) * 5) / 9);
+		}
+		function updateValues() {
+			temp_output_f.innerHTML = "Celcius temp: " + fToC(temp_input.value);
+			temp_output_c.innerHTML = "F temp: " + temp_input.value;
+		}
 
 
-	slider_input.addEventListener("change", function() {
-		temp_output.innerHTML = "Celcius temp: " + fToC(slider_input.value);
-		temp_output2.innerHTML = "F temp: " + slider_input.value;
+		temp_input.addEventListener("keyup", updateValues);
+		temp_input.addEventListener("click", updateValues);
 	});
-});
+}
