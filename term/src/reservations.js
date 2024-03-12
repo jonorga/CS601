20d1c4eb93781bd5e40b303d86ec5c189157a61d
jonorgan <jonorgan26@gmail.com>
2024-03-11 22:32:26 -0400

    select res within same month

--- term/src/reservations.js
+++ term/src/reservations.js
@@ -104,10 +104,11 @@ function setMonth(set_date) {
 }
 
 let selection_in_process = false;
+let selection_complete = false;
 function calendarEvent(event) {
+	if (selection_complete) return;
 	const target = event.currentTarget;
 	if (event.type == "click") {
-		// TODO: redo the selection process
 		if (!target.hasAttribute("selected") && !selection_in_process) {
 			target.style.backgroundColor = "#9F9F9F";
 			target.setAttribute("selected", "");
@@ -125,48 +126,19 @@ function calendarEvent(event) {
 			dates_selected.pop();
 			document.querySelector("#selections_p").innerHTML = "Date selected: none";
 			selection_in_process = false;
+		} else {
+			selection_complete = true;
+			target.style.backgroundColor = "#9F9F9F";
+			dates_selected.push(new Date(current_selection.year, current_selection.month, target.innerHTML));
+			const date1_f = months[dates_selected[0].getMonth()] + " " + cardinalDate(dates_selected[0].getDate()) + ", "
+				+ dates_selected[0].getFullYear();
+			const date2_f = months[dates_selected[1].getMonth()] + " " + cardinalDate(dates_selected[1].getDate()) + ", "
+				+ dates_selected[1].getFullYear();
+			const output = dates_selected[0] < dates_selected[1] ? date1_f + " - " + date2_f : date2_f + " - " + date1_f;
+			document.querySelector("#selections_p").innerHTML = "Date selected: " + output;
 		}
-
-
-
-		// if (target.hasAttribute("selected")) {
-		// 	if (target.hasAttribute("istoday"))
-		// 		target.style.backgroundColor = "#D4F0F8";
-		// 	else
-		// 		target.style.backgroundColor = "white";
-
-		// 	target.removeAttribute("selected");
-		// 	dates_selected.splice(dates_selected.indexOf(new Date(current_selection.year, current_selection.month, target.innerHTML)), 1);
-		// }
-		// else {
-		// 	target.setAttribute("selected", "");
-		// 	target.style.backgroundColor = "#9F9F9F";
-		// 	if (dates_selected.length == 0) {
-		// 		dates_selected.push(new Date(current_selection.year, current_selection.month, target.innerHTML));
-		// 		document.querySelector("#selections_p").innerHTML = "Date selected: " 
-		// 			+ months[dates_selected[0].getMonth()] + " " + cardinalDate(dates_selected[0].getDate()) + ", "
-		// 			+ dates_selected[0].getFullYear();
-		// 	}
-		// 	else if (dates_selected.length == 1) {
-		// 		dates_selected.push(new Date(current_selection.year, current_selection.month, target.innerHTML));
-		// 		const d1_formatted = months[dates_selected[0].getMonth()] + " " 
-		// 			+ cardinalDate(dates_selected[0].getDate()) + ", " + dates_selected[0].getFullYear();
-		// 		const d2_formatted = months[dates_selected[1].getMonth()] + " " 
-		// 			+ cardinalDate(dates_selected[1].getDate()) + ", " + dates_selected[1].getFullYear();
-
-		// 		if (dates_selected[0] < dates_selected[1]) {
-		// 			document.querySelector("#selections_p").innerHTML = "Date selected: " 
-		// 				+ d1_formatted + " - " + d2_formatted;
-		// 		}
-		// 		else {
-		// 			document.querySelector("#selections_p").innerHTML = "Date selected: " 
-		// 				+ d2_formatted + " - " + d1_formatted;
-		// 		}
-		// 	}
-		// }
 	}
 	else if (selection_in_process) {
-		// Every square between target and selected should have the 9F9F9F color
 		if (dates_selected[0].getMonth() == current_selection.month && dates_selected[0].getFullYear() == current_selection.year) {
 			const low_selected = dates_selected[0].getDate() <= target.innerHTML ? dates_selected[0].getDate() : Number(target.innerHTML);
 			const high_selected = dates_selected[0].getDate() > target.innerHTML ? dates_selected[0].getDate() : Number(target.innerHTML);
