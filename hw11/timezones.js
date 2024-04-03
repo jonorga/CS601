9ca2e0fbb4007966bf72af0529387a506df85f6c
jonorgan <jonorgan26@gmail.com>
2024-04-02 23:27:07 -0400

    started hw11

---
+++ hw11/timezones.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.pageFunction = void 0;
+function pageFunction() {
+    document.addEventListener("DOMContentLoaded", function () {
+        console.log("howdy world");
+    });
+}
+exports.pageFunction = pageFunction;
---
+++ hw11/timezones.ts
@@ -0,0 +1,5 @@
+export function pageFunction() {
+	document.addEventListener("DOMContentLoaded", function () {
+		console.log("howdy world");
+	});
+}
